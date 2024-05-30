ef540b463da11e4f0381487e2c4b87eb66425979
arthdarji352 <arthdarji20@gmail.com>
2024-05-30 17:46:56 +0530

    add auth middlware

---
+++ middlwares/authMiddleware.js
@@ -0,0 +1,17 @@
+import JWT from "jsonwebtoken";
+import userModel from "../models/userModel.js";
+
+// USER AUTH
+export const isAuth = async (req, res, next) => {
+  const { token } = req.cookies;
+  //valdiation
+  if (!token) {
+    return res.status(401).send({
+      success: false,
+      message: "UnAuthorized User",
+    });
+  }
+  const decodeData = JWT.verify(token, process.env.JWT_SECRET);
+  req.user = await userModel.findById(decodeData._id);
+  next();
+};
